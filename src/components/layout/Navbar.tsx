b67f69a7d81028bc1e710e76501522f98a9b4a0f
david-code-portfolio <ksendzulak.biz@gmail.com>
2026-03-16 17:15:58 +0100

    style: add navbar responsivity and hamburger menu

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -3,13 +3,20 @@ import Button from "../ui/button"
 
 function Navbar(){
     return(
-        <nav className="fixed top-8 left-1/2 -translate-x-1/2 bg-bg-surface pl-8 pr-2 py-2 items-center w-fit rounded-lg flex gap-8">
+        <nav className="fixed top-8 left-1/2 -translate-x-1/2 bg-bg-surface pl-8 pr-2 py-2 max-sm:py-4 max-sm:px-6 items-center w-fit rounded-lg flex gap-8 max-sm:min-w-[100px] max-sm:w-4/5 max-sm:justify-between">
             <ModeSwitchBtn></ModeSwitchBtn>
-            <a href='#'>Home</a>
-            <a href="#">Work</a>
-            <a href="#">Skills</a>
-            <a href="#">Process</a>
-            <Button></Button>
+            <div className="flex gap-8 max-sm:hidden">
+                <a href='#'>Home</a>
+                <a href="#">Work</a>
+                <a href="#">Skills</a>
+                <a href="#">Process</a>
+            </div>
+            <Button customStyle="max-sm:hidden"></Button>
+            {/* Hamburger menu (visible on mobile view) */}
+            <button className="relative w-6 h-4 cursor-pointer min-sm:hidden">
+                <div className="w-full h-1 bg-main rounded-lg absolute top-0"></div>
+                <div className="w-full h-1 bg-main rounded-lg absolute bottom-0"></div>
+            </button>
         </nav>
     )
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,7 @@
-function Button(){
+function Button( {customStyle} ){
     return(
         <button 
-            className="bg-main text-bg font-heading font-bold px-8 py-4 rounded-lg cursor-pointer leading-tight"
+            className={`${customStyle} bg-main text-bg font-heading font-bold px-8 py-4 rounded-lg cursor-pointer leading-tight`}
         >
             Contact
         </button>
