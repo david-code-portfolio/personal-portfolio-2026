cf1626e1a0a4aae9bbce254c0c2fd496cab2b2c3
david-code-portfolio <ksendzulak.biz@gmail.com>
2026-03-18 19:07:16 +0100

    navbar: add theme switch function

--- src/components/ui/modeSwitchBtn.tsx
+++ src/components/ui/modeSwitchBtn.tsx
@@ -1,10 +1,37 @@
+import { useState, useEffect } from 'react'
+
 import DarkModeIcon from '../../assets/svgs/dark-mode-icon.svg?react'
+import LightModeIcon from '../../assets/svgs/light-mode-icon.svg?react'
+import { div } from 'framer-motion/client'
 
 function ModeSwitchBtn() {
+    const [theme, setTheme] = useState(() => {
+        return localStorage.getItem('theme') === 'dark'
+    })
+    
+    useEffect(() => {
+        const html = document.documentElement
+        if(theme){
+            html.classList.add('dark')
+            localStorage.setItem('theme', 'dark')
+        }
+        else{
+            html.classList.remove('dark')
+            localStorage.setItem('theme', 'light')
+        }
+    })
+    
     return(
-        <DarkModeIcon 
-            className='cursor-pointer'
-        ></DarkModeIcon>
+        <div className='w-6 h-6 flex items-center justify-center'>
+            <DarkModeIcon 
+                className={`${theme === true ? `hidden` : ``} cursor-pointer`}
+                onClick={() => setTheme(theme => !theme)}    
+            />
+            <LightModeIcon
+                className={`${theme === true ? `` : `hidden`} cursor-pointer`}
+                onClick={() => setTheme(theme => !theme)}
+            />
+        </div>
     )
 }
 
